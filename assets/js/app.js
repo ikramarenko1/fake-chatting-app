733e23eb6d78447915186e635492944eb38458a3
Ilya Kramarenko <il.kramarenko1@gmail.com>
2023-04-14 20:59:28 +0300

    clean and comment js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ const avatarImage = './assets/images/avatar.jpg';
 
 // Сообщения, которые будет присылать фейковый пользователь
 const fakeMessages = [
-	'Hi there, it\'s the first message!',
+	'Hi there, it\'s the <img src="./assets/images.avatar.jpg"> first message!',
 	'Nice to meet you!',
 	'How are you?',
 	'Not too bad, thanks',
@@ -20,41 +20,38 @@ const fakeMessages = [
 	':)'
 ];
 
-let minutes = 0;
 
-// Инициализируем скроллбар и отображение сообщений в чате при загрузке страницы
+// Инициализация скроллбара и отображение первого сообщения при загрузке страницы
 $(window).on('load', function () {
-	messagesContent.mCustomScrollbar();
-	setTimeout(fakeMessage, 100);
+	messagesContent.mCustomScrollbar(); // добавляем mCSB к этому блоку
+	setTimeout(fakeMessage, 100); // вызываем отображение первого сообщения
 });
 
-// Добавим метку времени и прокрутку к низу
+
+// Обновление скроллбара, нужно для того, чтобы после каждого сообщения страница прокручивалась вниз, чтобы пользователь мог видеть последнее сообщение
 function updateScrollBar() {
 	messagesContent.mCustomScrollbar('update').mCustomScrollbar('scrollTo', 'bottom', {
-		scrollInertia: 10,
-		timeout: 0
+		scrollInertia: 10, // плавность прокрутки
+		timeout: 0 // скорость прокрутки
 	});
 };
 
+// Добавление времени к каждому сообщению в формате "HH:MM"
 function addTimeStamp() {
 	const date = new Date();
-	const minutesNow = date.getMinutes();
+	const timeStamp = $('<div class="timestamp"></div>').text(`${date.getHours()}:${date.getMinutes()}`); // создаем div.timestamp и пихаем в него текст в формате "HH:MM"
 
-	if (minutes !== minutesNow) {
-		minutes = minutesNow;
-
-		const timeStamp = $('<div class="timestamp"></div>').text(`${date.getHours()}:${minutes}`);
-
-		$('.message:last').append(timeStamp);
-	};
+	$('.message:last').append(timeStamp); // добавляем только последнему сообщению на странице
 };
 
-function addMessageToPage(msg, isPersonal = false) {
-	const message = $('<div class="message"></div>').text(msg);
 
-	if (isPersonal) {
+// Добавление сообщения на страницу (msg - сообщение, isPersonal - сообщение от фейка или юзера)
+function addMessageToPage(msg, isPersonal = false) {
+	const message = $('<div class="message"></div>').text(msg); // создаем div.message и пихаем в него текст msg
+
+	if (isPersonal) { // если сообщение от юзера то добавляем дополнительный класс для стилизации
 		message.addClass("message__personal");
-	} else {
+	} else { // в противном случае, добавляем фото фейк-пользователя к каждому сообщению
 		const figure = $('<figure class="avatar"></figure>');
 		const image = $('<img>').attr('src', avatarImage);
 
@@ -63,39 +60,41 @@ function addMessageToPage(msg, isPersonal = false) {
 		message.addClass('new').prepend(figure);
 	}
 
-	$('.mCSB_container').append(message);
+	$('.mCSB_container').append(message); // добавляем новое сообщение
 
 	addTimeStamp();
 	updateScrollBar();
 };
 
-// Вставляем сообщение пользователя
-function insertMessage() {
-	const messageText = messageInput.val().trim();
 
-	if (messageText === '') {
+// Добавление сообщения с поля ввода
+function insertMessage() {
+	const messageText = messageInput.val().trim(); // убираем лишние пробелы в начале/конце
+
+	if (messageText === '') { // если сообщение пустое - выходим
 		return false;
 	}
 
-	addMessageToPage(messageText, true);
-	messageInput.val(null);
-	setTimeout(fakeMessage, 1000 + (Math.random() * 20) * 100);
+	addMessageToPage(messageText, true); // вызываем функцию и указываем, что сообщение личное
+	messageInput.val(null); // очищаем поле для ввода
+	setTimeout(fakeMessage, 1000 + (Math.random() * 20) * 100); // таймер на вызов функции с сообщением от фейка
 };
 
-// Добавляем прослушку на инпут сообщения и нажатие на кнопку "Отправить"
+
+// Добавление прослушки на то, если пользователь нажмет ENTER при отправке
 messageInput.on('keydown', function(e) {
-	// Если пользователь нажал Enter - отправить сообщение
-	if (e.which === 13) {
+	if (e.which === 13) { // если пользователь нажал ENTER (код 13) - отправить сообщение
 		insertMessage();
 		return false;
 	}
 });
-
+// Добавление прослушки на то, если пользователь нажмет на кнопку "Отправить"
 messageSubmit.on('click', insertMessage);
 
-// Функция для отображения сообщения "загрузки" и смена на сообщение через 1-2 секунды
+
+// Отображение имитации печатания и смена на сообщение
 function fakeMessage() {
-	if (messageInput.val() !== '') {
+	if (messageInput.val() !== '') { // если поле для ввода не пустое - выходим
 		return false;
 	}
 
@@ -104,6 +103,7 @@ function fakeMessage() {
 	const image = $('<img>').attr('src', avatarImage);
 
 	figure.append(image);
+
 	loadingMessage.append(figure).append($('<span></span>'));
 	$('.mCSB_container').append(loadingMessage);
 
@@ -111,6 +111,6 @@ function fakeMessage() {
 
 	setTimeout(function () {
 		loadingMessage.remove();
-		addMessageToPage(fakeMessages.shift());
+		addMessageToPage(fakeMessages.shift()); // .shift() - возвращает первый элемент массива fakeMessages и удаляет его из массива.
 	}, 1000 + (Math.random() * 20) * 100);
 }
